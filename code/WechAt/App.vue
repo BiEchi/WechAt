41386cb011580778d1c943a89921d9a541c11114
Jackgetup <Haob.19@intl.zju.edu.cn>
2022-07-25 02:30:51 -0500

    sessions completed

--- code/WechAt/App.vue
+++ code/WechAt/App.vue
@@ -21,6 +21,6 @@
 
 </script>
 
-<style> 
+<style>
 	/*每个页面公共css */
 </style>
--- code/WechAt/pages/Chat_session/chat.vue
+++ code/WechAt/pages/Chat_session/chat.vue
@@ -1,89 +1,49 @@
 <!-- jiayuan is modifying -->
 <template>
 	<view class="content">
-		<div class="flex-container">
+		<!-- section for the public sessions -->
+		<!-- text to show that this is public session section -->
+		<view class="public">
+			<text class="public-text">Public Sessions</text>
+		</view>
+		<view v-for="pub_session in pub_sessions" :key="pub_session['Session_id']">
+			<div class="flex-container">
 			<div class="card">
 			  <img class="profile" src="/static/logo.png" alt="img1">
 			</div>
 			<div class="texting">
-				<h4 class="name"><b>{{name1}}</b></h4>
-				<p class="chatting">{{chat1}}</p>
+				<h4 class="name"><b>{{pub_session['Chat_name']}}</b></h4>
+				<p class="chatting">First Message</p>
 			</div>
 		</div>
-		<div class="flex-container">
-			<div class="card">
-			  <img class="profile" src="/static/logo.png" alt="img1">
+		</view>
+
+		<!-- section for the private sessions -->
+		<!-- text to show that this is private session section -->
+		<view class="private">
+			<text class="private-text">Private Sessions</text>
+		</view>
+		<view v-for="pri_session in pri_sessions" :key="pri_session['Session_pri_id']">
+			<div class="flex-container">
+				<div class="card">
+				<img class="profile" src="/static/logo.png" alt="img1">
+				</div>
+				<div class="texting">
+					<h4 class="name"><b>Friend Name</b></h4>
+					<p class="chatting">First message TBD.</p>
+				</div>
 			</div>
-			<div class="texting">
-				<h4 class="name"><b>{{name2}}</b></h4>
-				<p class="chatting">{{chat2}}</p>
-			</div>
-		</div>
-		</div> 
-		<div class="flex-container">
-			<div class="card">
-			  <img class="profile" src="/static/logo.png" alt="img1">
-			</div>
-			<div class="texting">
-				<h4 class="name"><b>{{name3}}</b></h4>
-				<p class="chatting">{{chat3}}</p>
-			</div> 
-		</div>
-		</div>
-		<div class="flex-container">
-			<div class="card">
-			  <img class="profile" src="/static/logo.png" alt="img1">
-			</div>
-			<div class="texting">
-				<h4 class="name"><b>{{name4}}</b></h4>
-				<p class="chatting">{{chat4}}</p>
-			</div>
-		</div>
-		<div class="flex-container">
-			<div class="card">
-			  <img class="profile" src="/static/logo.png" alt="img1">
-			</div>
-			<div class="texting">
-				<h4 class="name"><b>{{name5}}</b></h4>
-				<p class="chatting">{{chat5}}</p>
-			</div>
-		</div>
-		<div class="flex-container">
-			<div class="card">
-			  <img class="profile" src="/static/logo.png" alt="img1">
-			</div>
-			<div class="texting">
-				<h4 class="name"><b>{{name6}}</b></h4>
-				<p class="chatting">{{chat6}}</p>
-			</div>
-		</div>
+		</view>
 	</view>
 
 </template>
 
 <script>
-	// console.log("Hello World!")
-	// window.alert()
 	export default {
 		data() {
 			return {
-				name1: "CS 411",
-				name2: "CS 374",
-				name3: "ECE 391",
-				name4: "LAS 100",
-				name5: "CS 446",
-				name6: "411 Summer",
-				chat1: "Let have midterm demo",
-				chat2: "Final is coming",
-				chat3: "plagiarism detected",
-				chat4: "Useless",
-				chat5: "ML is good!",
-				chat6: "DDL AGAIN!!!",
-				
-				
-				
-				session_id_list: {},
-				my_user_id: 1 
+				pub_sessions: [],
+				pri_sessions: []
 			} 
 		},
 		onLoad() {
@@ -100,23 +60,12 @@
 				uniCloud.callFunction({
 					name: 'query',
 					data: {
-						sentence: 'SELECT * FROM Joined WHERE User_id = ?',
+						sentence: 'SELECT * FROM Joined NATURAL JOIN Chat_session WHERE User_id = ?',
 						arguments: [this.my_user_id]
 					},
-				
 					success: res => {
+						this.pub_sessions = res.result
 						console.log('kill me in uniCloud success 1')
-						console.log(res.result)
-						// if (this.passwords!=res.result[0]['User_password']){//!!not sure
-						// 	this.register()
-						// }
-						// else {
-						// 	this.user_id = res.result[0]['User_id']
-						// 	getApp().globalData.user_email = this.email
-						// 	getApp().globalData.user_id = this.user_id 
-						// 	console.log(getApp().globalData.user_id)
-						// 	this.$router.push('/pages/account/account')
-						// }
 					},					
 					fail: err=>{
 						console.log(err)
@@ -127,22 +76,11 @@
 					name: 'query',
 					data: {
 						sentence: 'SELECT * FROM Joined_pri WHERE User1_id = ? OR User2_id = ?',
-						arguments: [this.my_user_id]
+						arguments: [this.my_user_id, this.my_user_id]
 					},
-				
 					success: res => {
+						this.pri_sessions = res.result
 						console.log('kill me in uniCloud success 2')
-						console.log(res.result)
-						// if (this.passwords!=res.result[0]['User_password']){//!!not sure
-						// 	this.register()
-						// }
-						// else {
-						// 	this.user_id = res.result[0]['User_id']
-						// 	getApp().globalData.user_email = this.email
-						// 	getApp().globalData.user_id = this.user_id 
-						// 	console.log(getApp().globalData.user_id)
-						// 	this.$router.push('/pages/account/account')
-						// }
 					},					
 					fail: err=>{
 						console.log(err)
